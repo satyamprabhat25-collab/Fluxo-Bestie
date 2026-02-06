5aba1b65c4861f6d46cf47a95226a58a98e6ab3b
satyamprabhat25-collab <satyamprabhat25@gmail.com>
2026-02-06 21:25:08 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: "./", 
+  // Yahan "./" ko hata kar "/" kar diya hai
+  base: "/", 
   server: {
     host: "::",
     port: 8080,
